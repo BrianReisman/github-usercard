613bd176ce2560794d4a5dcbbaef49ee15a8d123
Brian Reisman <brianreisman@protonmail.com>
2020-10-29 15:51:45 -0400

    start building out component maker

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,9 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+import axios from 'axios'
+axios
+  .get('https://api.github.com/users/brianreisman')
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -30,6 +33,41 @@
 
 const followersArray = [];
 
+function cardMaker(){
+//creating elements
+const divCard = document.createElement('div')
+const image = document.createElement('img')
+const cardInfo = document.createElement('div')
+const h3Name = document.createElement('h3')
+const pUsername = document.createElement('p')
+const pLocation = document.createElement('p')
+const pProfile = document.createElement('p')
+const ahref = document.createElement('a')
+const pFollowers = document.createElement('p')
+const pFollowing = document.createElement('p')
+const pBio = document.createElement('p')
+//heirarchy
+divCard.appendChild(image)
+divCard.appendChild(cardInfo)
+cardInfo.appendChild(h3Name)
+cardInfo.appendChild(pUsername)
+cardInfo.appendChild(pLocation)
+cardInfo.appendChild(pProfile)
+pProfile.appendChild(ahref)
+cardInfo.appendChild(pFollowers)
+cardInfo.appendChild(pFollowing)
+cardInfo.appendChild(pBio)
+//add class and attributes
+divCard.classList('card')
+cardInfo.classList('card-info')
+h3Name.classList('name')
+pUsername.classList('username')
+
+return divCard
+}
+
+console.log('hi')
+
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
@@ -49,6 +87,9 @@ const followersArray = [];
       </div>
     </div>
 */
+
+
+
 
 /*
   List of LS Instructors Github username's:
